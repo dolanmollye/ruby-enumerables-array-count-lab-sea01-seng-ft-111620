257f9f872e5ffbfc6dfa2dec1d6475f2fe7c028d
Molly Dolan <dolanmollye@gmail.com>
2020-11-04 18:45:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,6 @@
+# Return the total number of strings in the provided array using the count enumerabl
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  
 end
 
 def count_empty_strings(array)
